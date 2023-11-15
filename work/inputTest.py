bb48f9e4e497d1d6edaed63f4b3ce284a2f4182d
Bolaji Adesina <39880314+Bolajiadesina@users.noreply.github.com>
2023-11-15 21:31:10 +0000

    coding in python

---
+++ work/inputTest.py
@@ -0,0 +1,6 @@
+birth_year = input ("Enter your birth year : ")
+age = 2020 - int(birth_year)
+print(age)
+
+
+
---
+++ work/test.py
@@ -0,0 +1,10 @@
+msg= "Hello world"
+price = "19.56"
+first_name= "Bolaji"
+is_online= False
+print(is_online)
+
+print (first_name +" is saying "+ msg+ " at "+ price )
+
+name = input("What is your name? ")
+print ("Hello "+ name)
